3bb96a71af76a69753e322050378dd35b9f5930e
mohsinchd <mohsinshoaib735@gmail.com>
2025-03-05 22:49:56 +0500

    Revert "Merge pull request #1 from NoorFatima-developer/database-connection"
    This reverts commit 818f87647f4e6c2c2d81d37a2323397356832dde, reversing
    changes made to c657645787c88d15611b2aeb140e5d00e0097d16.

--- server.js
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import app from "./app.js";
-import { connectDB } from "./config/db.js";
-
-connectDB();
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on: ${process.env.PORT}`);
-});
